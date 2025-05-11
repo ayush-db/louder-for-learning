6809f3c761e374f3268aae0576f4922545357ab4
ayush-db <ayushdebbhattacharyya@gmail.com>
2025-05-11 19:22:14 -0400

    updated 5/11/24

--- frontend/src/homepage.jsx
+++ frontend/src/homepage.jsx
@@ -1,1 +1,9 @@
-console.log('hello world')
+import React from 'react';
+export default function Homepage() {
+    return (
+        <div>
+        <h1>Welcome to the Homepage</h1>
+        <p>This is the main page of the application.</p>
+        </div>
+    );
+    }
