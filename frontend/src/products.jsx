2ada20ad8ed259a06b6f80e5f4b6161c9c2da71b
ayush-db <ayushdebbhattacharyya@gmail.com>
2025-07-25 12:18:55 -0400

    7/25/2025 (with products.jsx)

---
+++ frontend/src/products.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Layout from './layout.jsx';
+export default function Products() {
+     return (
+            <Layout>
+            <div className = "text-gray-800"> 
+                <section className="flex justify-center my-10">
+                    <div>
+                        <h2>What We Offer</h2>
+                        <p>Louder For Learning offers several resources that can help teachers in underfunded school districts - these include grant proposals, sites that can provide cheaper textbooks and other supplies, and ways to get in contact with representatives that could potentially help.</p>
+                    </div>
+                    
+                </section>
+        }
