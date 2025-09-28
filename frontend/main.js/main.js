155d5da0767d0d735c5014c4e048135e440de91a
ayush-db <ayushdebbhattacharyya@gmail.com>
2025-09-28 17:26:19 -0400

    updated start of 9/28/25

--- frontend/main.js/main.js
+++ frontend/main.js/main.js
@@ -1,14 +1,26 @@
 const {Client}=require('pg');
 
-const con=new Client({
+const client=new Client({
     host:"localhost", 
     user:"postgres",
     port:5432,
     password:"your_db_password",
     database:"db"
-})
-
-con.connect().then(()=>console.log("connected"))
+});
+(async()=>{
+    try {
+        await client.connect();
+        console.log("connected to db");
+        const res=await client.query('Select * from table');
+        console.log(res.rows);
+    } catch (err) {
+        console.log(err.message);
+    } })();
+/*con.connect()
+//.then(()=>{
+    console.log("connected")
+}
+.catch(err=>console.log(err.message));
 
 con.query('Select * from table', (err, ress ))=>{
     if(!err){
@@ -17,4 +29,4 @@ con.query('Select * from table', (err, ress ))=>{
         console.log(err.message)
     }
     con.end();
-}
+}/*
---
+++ frontend/src/articles.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Navbar from './navbar.jsx';
+export default function Articles() {
+    return (
+        <>
+        <Navbar />
+        <section>
+            <div>
+                <h1>Articles</h1>
+            </div>
+        </section>
+        <article>
+            <h2>Placeholder</h2>
+            <p>hjefhuilfids</p>
+        </article>
+        </>
+    );}
